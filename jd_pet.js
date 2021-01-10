d3333a8b1a78ed196ff57c81b10a1735f9a1a5ca
kinson90 <76594148+kinson90@users.noreply.github.com>
2021-01-10 21:17:58 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -1,6 +1,6 @@
 /*
 东东萌宠 更新地址： https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pet.js
-更新时间：2021-01-06
+更新时间：2021-01-10
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 
@@ -263,8 +263,8 @@ async function masterHelpInit() {
  * 运行脚本时你自己的shareCode会在控制台输出, 可以将其分享给他人
  */
 async function slaveHelp() {
-  $.log(`\n因1.6日好友助力功能下线。故暂时屏蔽\n`)
-  return
+  //$.log(`\n因1.6日好友助力功能下线。故暂时屏蔽\n`)
+  //return
   let helpPeoples = '';
   for (let code of newShareCodes) {
     console.log(`开始助力京东账号${$.index} - ${$.nickName}的好友: ${code}`);
@@ -472,8 +472,8 @@ function shareCodesFormat() {
       newShareCodes = shareCodes[tempIndex].split('@');
     }
     //因好友助力功能下线。故暂时屏蔽
-    // const readShareCodeRes = await readShareCode();
-    const readShareCodeRes = null;
+    const readShareCodeRes = await readShareCode();
+    //const readShareCodeRes = null;
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       newShareCodes = [...new Set([...newShareCodes, ...(readShareCodeRes.data || [])])];
     }
