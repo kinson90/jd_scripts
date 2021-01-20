04d5bef38b3a2938c581eacd9051431495d60c91
kinson90 <76594148+kinson90@users.noreply.github.com>
2021-01-20 17:54:27 +0800

    Update jd_xxl.js

--- jd_xxl.js
+++ jd_xxl.js
@@ -34,8 +34,10 @@ let ACT_ID = 'A_112790_R_1_D_20201028'
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let inviteCodes = [
-  '',
-  '',
+  '2845317@2845449@2845582',
+  '2519805@2845449@2845582',
+  '2519805@2845317@2845582',
+  '2519805@2845317@2845449'
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
